68ef8304d786df0f18ce69ec66b0b0593305c3ab
Vamsi <pusarlatarunvamsi@gmail.com>
2024-01-03 16:39:24 +0530

    updated mongoose connection string

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://kirags123:8qPEa8KTKBEh2bss@cluster0.f3qlbuo.mongodb.net/todos")
+mongoose.connect("mongodb+srv://vamsik1643:vamsik1643@cluster0.bjo7qbt.mongodb.net/")
 const todoSchema = mongoose.Schema({
     title: String,
     description: String,
